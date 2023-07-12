088ecde55f52c628b36ce25569fb50713334346b
Oxyclean114 <dullenj@gmail.com>
2023-07-12 13:56:38 -0400

    Modified movement and added an AnimatedSprite3D node

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,6 +2,9 @@ extends CharacterBody3D
 
 @export var speed = 14
 @export var fall_acceleration = 75
+
+@onready var animation_player = get_node("AnimatedSprite3D")
+
 var target_velocity = Vector3.ZERO
 
 func _physics_process(delta):
@@ -9,15 +12,43 @@ func _physics_process(delta):
 
 #Standard input detection.
 #NOTE: does not allow for joystick analog inputs.
+#---------------------------------------------------------
 	if Input.is_action_pressed("move_right"):
 		direction.x += 1
+		animation_player.flip_h = false
+		animation_player.play()
+	if Input.is_action_just_released("move_right"):
+		animation_player.stop()
+#---------------------------------------------------------
 	if Input.is_action_pressed("move_left"):
 		direction.x -= 1
-	if Input.is_action_pressed("move_back"):
+		animation_player.flip_h = true
+		animation_player.play()
+	if Input.is_action_just_released("move_left"):
+		animation_player.stop()
+#---------------------------------------------------------
+	if Input.is_action_pressed("move_down"):
 		direction.z += 1
-	if Input.is_action_pressed("move_forward"):
+		animation_player.play()
+	if Input.is_action_just_released("move_down"):
+		animation_player.stop()
+#---------------------------------------------------------
+	if Input.is_action_pressed("move_up"):
 		direction.z -= 1
+		animation_player.play()
+	if Input.is_action_just_released("move_up"):
+		animation_player.stop()
+#---------------------------------------------------------
 
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 		$Pivot.look_at(position + direction, Vector3.UP)
+		
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+	
+	if not is_on_floor():
+		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+		
+	velocity = target_velocity
+	move_and_slide()
