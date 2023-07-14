e13f87c7b91fddc3c1e92b5a3d71ceaae5dac708
HeartBox12 <maximosity1@gmail.com>
2023-07-13 20:10:13 -0700

    NPC Progress
    NPC now has specific sprite and timeline.

--- Player/Player.gd
+++ Player/Player.gd
@@ -56,4 +56,5 @@ func _physics_process(delta):
 	
 	if Input.is_action_just_pressed("ui_accept"):
 		if $Pivot/NPCDetector.get_collision_count() != 0:
-			print("Found it!")
+			Dialogic.start($Pivot/NPCDetector.get_collider(0).get_parent().timeline) # Reference target's timeline
+			#NOTE: this method is meant to reference the string path to that resource.
