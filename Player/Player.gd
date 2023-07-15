8d320fc8b14000f71b350fcb53d8912b14af2e09
HeartBox12 <maximosity1@gmail.com>
2023-07-15 11:13:30 -0700

    Dialogue freeze
    No mor eplayer input while in dialogue.

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,6 +8,12 @@ extends CharacterBody3D
 var target_velocity = Vector3.ZERO
 
 func _physics_process(delta):
+	
+	#NO PLAYER INPUT OR MOVEMENT WHILE IN DIALOGUE
+	if Dialogic.current_timeline != null:
+		
+		return
+	
 	var direction = Vector3.ZERO
 	animation_player.play()
 
@@ -58,5 +64,4 @@ func _physics_process(delta):
 	
 	if Input.is_action_just_pressed("ui_accept"):
 		if $Pivot/NPCDetector.get_collision_count() != 0:
-			pass
-			#Dialogic.start($Pivot/NPCDetector.get_collider(0).get_parent().timeline) # Reference target's timeline
+			Dialogic.start($Pivot/NPCDetector.get_collider(0).get_parent().timeline) # Reference target's timeline
