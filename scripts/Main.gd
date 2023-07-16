7bd2e8d52732c9bbf0a5b9225cee6dcda23ea749
Oxyclean114 <dullenj@gmail.com>
2023-07-16 14:35:55 -0400

    Camera changes, fall safety, esc to exit

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -2,18 +2,19 @@ extends Node3D
 
 @export var FMODEvent:EventAsset
 
-
 var instance:EventInstance
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var dests = [$dest0, $dest1, $dest2,]
 	$Player.position = dests[Singleton.destId].position
-	
 	instance = RuntimeManager.create_instance(FMODEvent)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
+	if Input.is_action_just_pressed("exit_game"):
+		get_tree().quit()
 	pass
 
 func _unhandled_input(event):
