abd74e65de5f8a0d1bdfb451ba01096dfabd3ffc
devinchin <121265336+devinchin@users.noreply.github.com>
2023-07-21 12:48:08 -0700

    generate fmod guids

---
+++ addons/FMOD/editor/fmod_guids.gd
@@ -0,0 +1,23 @@
+#
+   # fmod_guids.gd - FMOD Studio API
+
+   # Generated GUIDs for project 'last day at work.fspro'
+#
+
+class_name FMODGuids
+
+class Events:
+	const MUSIC_MUSIC = "{f1a5ab77-b127-473b-a04a-a0203c84df46}"
+
+class Busses:
+	const MASTER_BUS = "{9adf7eed-0fcb-40fc-9273-c2e931e41b0e}"
+	const REVERB = "{00da4684-8870-4fcd-8e7d-e2559e5ac6cc}"
+
+class Banks:
+	const MASTER = "{91d6e2dc-7a18-43ba-ac47-0d48b26e86de}"
+	const MUSIC_BANK = "{f3f3abc1-4516-45dd-bd5d-a690dcec9401}"
+
+class Parameters:
+	const CHARACTER = "{b4576f9b-2c73-4f1d-b215-21a3ff8c13d9}"
+	const PAUSED = "{ffb36504-280d-48c6-a28b-050e313198d7}"
+
