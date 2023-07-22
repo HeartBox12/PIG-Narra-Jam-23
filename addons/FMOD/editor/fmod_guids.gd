d9d1846d78454df6ca1ae86fe5b26c2906fae641
devinchin <121265336+devinchin@users.noreply.github.com>
2023-07-21 20:36:43 -0700

    add sfx: sounds bank and associated guids

--- addons/FMOD/editor/fmod_guids.gd
+++ addons/FMOD/editor/fmod_guids.gd
@@ -8,6 +8,12 @@ class_name FMODGuids
 
 class Events:
 	const MUSIC_MUSIC = "{f1a5ab77-b127-473b-a04a-a0203c84df46}"
+	const SOUNDS_DOOR_CLOSE = "{666343d0-ae1a-4f75-95de-777f7c3a53b9}"
+	const SOUNDS_DOOR_OPEN = "{b6e63a4b-0520-4b37-9abc-892caa3038fd}"
+	const SOUNDS_ELECTRIC_HUM = "{0085bd8c-c25f-4d51-a489-2d43c7ba6f00}"
+	const SOUNDS_FOOTSTEP = "{a487f7af-bd88-494b-a857-2876c3c10ef6}"
+	const SOUNDS_ROOM_TONE = "{628c977a-f06c-4d96-b40c-10b51bf99aa7}"
+	const SOUNDS_STREET = "{d0000d6d-3c88-4969-bd73-782fd8d68200}"
 
 class Busses:
 	const MASTER_BUS = "{9adf7eed-0fcb-40fc-9273-c2e931e41b0e}"
@@ -16,6 +22,7 @@ class Busses:
 class Banks:
 	const MASTER = "{91d6e2dc-7a18-43ba-ac47-0d48b26e86de}"
 	const MUSIC_BANK = "{f3f3abc1-4516-45dd-bd5d-a690dcec9401}"
+	const SOUNDS_BANK = "{9c4cf937-3557-4b33-abb4-8e300f3b14e8}"
 
 class Parameters:
 	const CHARACTER = "{b4576f9b-2c73-4f1d-b215-21a3ff8c13d9}"
