c67e02615baa7c901d4285bd451de2c472a7e4c5
Oxyclean114 <dullenj@gmail.com>
2023-07-24 00:45:43 -0400

    Push for Max

---
+++ scenes/screenBlur.gd
@@ -0,0 +1,9 @@
+extends ColorRect
+
+
+@onready var blur = $Blur
+@export var blur_amount = 10
+
+func _process(delta):
+	blur_amount = wrapf(blur_amount + 0.05, 0.0, 5.0)
+	blur.material.set_shader_param("blur_amount", blur_amount)
