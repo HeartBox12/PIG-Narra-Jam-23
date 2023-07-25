8ac2c297bc29bf5c614395850445b9497a9d3ba6
Oxyclean114 <dullenj@gmail.com>
2023-07-25 01:12:27 -0400

    ShapeCast3D

--- scripts/ExitStageLeft.gd
+++ scripts/ExitStageLeft.gd
@@ -5,7 +5,7 @@ var openCall:Callable = Callable(self, "open_done")
 var openInst
 var closeInst
 
-func _on_body_entered(body):
+func _area_entered(body):
 	print("Detection") #Not detecting
 	openInst = FMODStudioModule.get_studio_system().get_event_by_id(FMODGuids.Events.SOUNDS_DOOR_OPEN).create_instance()
 	#variable definition from hell
