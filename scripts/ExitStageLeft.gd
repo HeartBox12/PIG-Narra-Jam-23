24e4b6fd0990c7ebd7617a290ff3dbf0cf0f315a
HeartBox12 <maximosity1@gmail.com>
2023-07-24 21:43:09 -0700

    Attempted area3d fix
    It didn't work

--- scripts/ExitStageLeft.gd
+++ scripts/ExitStageLeft.gd
@@ -5,12 +5,16 @@ var openCall:Callable = Callable(self, "open_done")
 var openInst
 var closeInst
 
+
+
 func _on_body_entered(body):
+	pass
+	
+func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
 	print("Detection") #Not detecting
 	openInst = FMODStudioModule.get_studio_system().get_event_by_id(FMODGuids.Events.SOUNDS_DOOR_OPEN).create_instance()
 	#variable definition from hell
 	openInst.start()
-	
 	openInst.set_callback(openCall, FMODStudioModule.FMOD_STUDIO_EVENT_CALLBACK_TIMELINE_MARKER)
 
 func open_done():
@@ -18,3 +22,6 @@ func open_done():
 	closeInst = FMODStudioModule.get_studio_system().get_event_by_id(FMODGuids.Events.SOUNDS_DOOR_CLOSE).create_instance()
 	closeInst.start()
 	closeInst.release()
+
+
+	
