eaafb0e163d8e41a93400380c76bb53d38c681c0
HeartBox12 <maximosity1@gmail.com>
2023-07-24 21:48:45 -0700

    Sequence fleshing-out
    If only the thing would start.

--- scripts/ExitStageLeft.gd
+++ scripts/ExitStageLeft.gd
@@ -9,13 +9,17 @@ var closeInst
 
 func _on_body_entered(body):
 	pass
-	
+
 func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
 	print("Detection") #Not detecting
+	get_node("../../player").canMove = false #The player should "look gone"
+	get_node("../../player").visible = false
 	openInst = FMODStudioModule.get_studio_system().get_event_by_id(FMODGuids.Events.SOUNDS_DOOR_OPEN).create_instance()
 	#variable definition from hell
 	openInst.start()
 	openInst.set_callback(openCall, FMODStudioModule.FMOD_STUDIO_EVENT_CALLBACK_TIMELINE_MARKER)
+	
+	$"/root/RuntimeManager".start() #close the game after 10s
 
 func open_done():
 	openInst.release()
@@ -24,4 +28,5 @@ func open_done():
 	closeInst.release()
 
 
-	
+func _on_timeout():
+	get_tree().quit()
